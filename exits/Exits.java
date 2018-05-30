86c41a934d0759378f83d6799931a14d3f9f4041
Stijn <stijn.schildermans@kuleuven.be>
2018-05-30 18:44:56 +0200

    Page faults optimized

--- exits/Exits.java
+++ exits/Exits.java
@@ -1,18 +1,31 @@
-public class Exits{
+import java.util.ArrayList;
 
-	private static int[] cache;
+public class Exits{
+	private static int[][] buf;
+	private static ArrayList<Integer> cache;
 
 	public static void main(String[] args){
-		cache = new int[10000000];
+		int size = Integer.parseInt(args[0]);
+		buf = new int[100000][];
+		/*cache = new ArrayList();
 
-		for (int i = 0; i<10000000;i+=1024){
-			cache[i]++;
-		}
+		for (int i = 0; i<size*5000000;i++){
+			cache.add(i);
+		}*/
+		faults(size);
 
-		System.out.println(cache[(int)(Math.random()*10000000)]);
+		//System.out.println(cache.get((int)(Math.random()*size*5000000)));
+	   System.out.println(buf[((int)Math.random())*100000][((int)Math.random())*1024]);
 
 	}
-
+	
+	private static void faults(int size){
+		
+		for (int i = 0; i  < size*75000; i++){
+			 buf[i%100000] = new int[1024];
+		}
+		cache.add(100);
+	}
 
 
 
