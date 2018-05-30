4b51092b7dbb4c88c8763d59627e59421058b71a
Stijn Schildermans <stijn.s95@gmail.com>
2018-05-30 19:08:27 +0200

    Faults gemaakt

--- exits/Exits.java
+++ exits/Exits.java
@@ -5,26 +5,26 @@ public class Exits{
 	private static ArrayList<Integer> cache;
 
 	public static void main(String[] args){
-		int size = Integer.parseInt(args[0]);
-		buf = new int[100000][];
+		//int size = Integer.parseInt(args[0]);
+		buf = new int[50000][];
 		/*cache = new ArrayList();
 
 		for (int i = 0; i<size*5000000;i++){
 			cache.add(i);
 		}*/
-		faults(size);
+		faults();
 
 		//System.out.println(cache.get((int)(Math.random()*size*5000000)));
-	   System.out.println(buf[((int)Math.random())*100000][((int)Math.random())*1024]);
+	   System.out.println(buf[((int)Math.random())*50000][((int)Math.random())*1024]);
 
 	}
 	
-	private static void faults(int size){
+	private static void faults(){
 		
-		for (int i = 0; i  < size*75000; i++){
-			 buf[i%100000] = new int[1024];
+		for (int i = 0; i  < 50000; i++){
+			 buf[i] = new int[1024];
 		}
-		cache.add(100);
+		//cache.add(100);
 	}
 
 
