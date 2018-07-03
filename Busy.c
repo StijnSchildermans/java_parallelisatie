be3078c3e598b73809a3ad5946fda2fb47f2ec5c
Stijn <stijn.schildermans@kuleuven.be>
2018-07-03 10:50:05 +0200

    Busy loop added

---
+++ Busy.c
@@ -0,0 +1,8 @@
+int x;
+
+int main(){
+	while(1){
+		if (x==1000) x= 0;
+		else x++;
+	}
+}
--- call.sh
+++ call.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "threadPool 10000" | netcat 192.168.122.180 6666 -q 10000
+echo "threadPool 100" | netcat 192.168.122.180 6666 -q 10000
